e9565e5c9d9d13b3c871415201bacb6d7f57a602
Partab Raj <64382440+smmartprogrammer@users.noreply.github.com>
2021-08-21 12:45:05 +0500

    Add files via upload

---
+++ gold/app.js
@@ -0,0 +1,34 @@
+
+
+
+// async function fetchData() {
+//     const response = await fetch("https://metals-api.com/api/latest?access_key=88mblef0aaiiuj5zmafdtinn9yvy76cgb49ri64116dx08pnf0xibw6axp5b&base=EUR&symbols=XAU%2CXAG%2CXPD%2CXPT%2CXRH");
+//     console.log("response", response)
+
+//     const data = await response.json();
+//     console.log("Data", data)
+
+//     var date = document.getElementById("date")
+//     date.innerHTML = `date ${data.date}`
+
+//     var base = document.getElementById("base")
+//     base.innerHTML = `base ${data.base}`
+
+//     var rates = document.getElementById("rates")
+//     rates.innerHTML = `rates ${data.rates.XAU / 28.35}`
+
+
+//     var coins = document.getElementById("900coints")
+//     coins.innerHTML = `900coins ${data.rates.XAU / 1000 * 900}`
+
+//     var gold = document.getElementById("gold")
+//     gold.innerHTML = `750gold ${data.rates.XAU / 1000 * 750}`
+
+//     var fiveeightfive = document.getElementById("fiveeightfive")
+//     fiveeightfive.innerHTML = `585gold ${data.rates.XAU / 1000 * 585}`
+
+//     var thirtythree = document.getElementById("thirtythree")
+//     thirtythree.innerHTML = `333gold ${data.rates.XAU / 1000 * 333}`
+
+// }
+// fetchData();
